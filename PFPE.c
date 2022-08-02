59e8a6ef2d014c381144f2a29eb34c19f58b7674
ByRCx <49207371+ByRCx@users.noreply.github.com>
2022-08-01 19:36:43 -0500

    Update PFPE.c

--- PFPE.c
+++ PFPE.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-#include <conio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <conio.h>
 #include <string.h>
+#include <ctype.h>
 #include <math.h>
 
 
@@ -11,15 +13,16 @@ int main()
     
     printf("Proyecto Final de Programación estructurada\n");
     
+    
     int z = 0;
     int y;
     int pesos;
     float pesosf;
-    char ch;
+    char ch[1];
     
 
     while (z != 3){
-        
+       
     printf("\nSeleccione El programa a correr \n");
     printf("1- Dispensador de billetes \n");
     printf("2- Juego de basketball\n");
@@ -31,35 +34,37 @@ int main()
     system("clear");
     printf("Dispensador de billetes \n");
     printf("inserte la cantidad a retirar del cajero: \n"), scanf("%f", &pesosf);
-    printf("[round(%f)] Billete/s de 2000 \n", pesosf / 2000, round(pesosf));
-    pesosf = round(fmod(pesosf, 2000));
-    printf("[%f] Billete/s de 1000 \n", pesosf / 1000);
+    printf("%.0f Billete/s de 2000 \n", truncf(pesosf / 2000));
+    pesosf = fmod(pesosf, 2000);
+    printf("%.0f Billete/s de 1000 \n", truncf(pesosf / 1000));
     pesosf = fmod(pesosf, 1000);
-    printf("[%f] Billete/s de 500 \n", pesosf / 500);
+    printf("%.0f Billete/s de 500 \n", truncf(pesosf / 500));
     pesosf = fmod(pesosf, 500);
-    printf("[%f] Billete/s de 200 \n", pesosf / 200);
+    printf("%.0f Billete/s de 200 \n", truncf(pesosf / 200));
     pesosf = fmod(pesosf, 200);
-    printf("[%f] Billete/s de 100 \n", pesosf / 100);
+    printf("%.0f Billete/s de 100 \n", truncf(pesosf / 100));
     pesosf = fmod(pesosf, 100);
-    printf("[%f] Billete/s de 50 \n", pesosf / 50);
+    printf("%.0f Billete/s de 50 \n", truncf(pesosf / 50));
     pesosf = fmod(pesosf, 50);
-    printf("[%f] Modena/s de 25 \n", pesosf / 25);
+    printf("%.0f Modena/s de 25 \n", truncf(pesosf / 25));
     pesosf = fmod(pesosf, 25);
-    printf("[%f] Modena/s de 10 \n", pesosf / 10);
+    printf("%.0f Modena/s de 10 \n", truncf(pesosf / 10));
     pesosf = fmod(pesosf, 10);
-    printf("[%f] Modena/s de 5 \n", pesosf / 5);
+    printf("%.0f Modena/s de 5 \n", truncf(pesosf / 5));
     pesosf = fmod(pesosf, 5);
-    printf("[%f] Modena/s de 1 \n", pesosf / 1);
+    printf("%.0f Modena/s de 1 \n", truncf(pesosf / 1));
     pesosf = fmod(pesosf, 1);
-    printf("[%f] Modena/s de 25 centavos\n", pesosf / 0.25);
+    printf("%.0f Modena/s de 25 centavos\n", truncf(pesosf / 0.25));
     pesosf = fmod(pesosf, 0.25);
-    printf("[%f] Modena/s de 10 centavos\n", pesosf / 0.10);
+    printf("%.0f Modena/s de 10 centavos\n", truncf(pesosf / 0.10));
     pesosf = fmod(pesosf, 0.10);
-    printf("[%f] Modena/s de 5 centavos\n", pesosf / 0.05);
+    printf("%.0f Modena/s de 5 centavos\n", truncf(pesosf / 0.05));
     pesosf = fmod(pesosf, 0.05); 
-    printf("[%f] chicle/s como no hay de 1 centavo/s \n", pesosf / 0.01);
+    printf("%.0f chicle/s como no hay de 1 centavo/s \n", round(pesosf / 0.01));
     pesosf = fmod(pesosf, 0.01);
-    printf("\n Presione cualquier boton para continuar\n"), scanf("%c, &ch");
+    printf("\n Espere para continuar\n"); 
+    sleep(10);
+    system("clear");
     break;
     
     case 2:
@@ -77,9 +82,14 @@ int main()
     break;
     
     default:
+    
     system("clear");
-    printf("Esta opcion no es valida pulse cualquier tecla para continuar");
-    //scanf();
+    printf("\n Espere para continuar...\n"), sleep(1);
+     system("clear");
+    printf("\n Espere para continuar..\n"), sleep(1);
+     system("clear");
+    printf("\n Espere para continuar.\n"), sleep(1);
+    system("clear");
     break;
     }
     }
